0c52a6cac25976a6eb652b90e61cb326cc1ef331
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-16 00:28:36 +0530

    Added: functionality to check max of three integer

--- Max_of_three.java
+++ Max_of_three.java
@@ -2,8 +2,18 @@ package maxOfThree;
 
 public class Max_of_three {
 	
+	public static Integer maximum(Integer val1, Integer val2, Integer val3) {
+		int max= val1;
+		if(val2.compareTo(max)>0) {
+			max=val2;
+		}else if(val3.compareTo(max)>0) {
+			max=val3;
+		}
+		return max;
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to generic problem");
+		Integer val1=20, val2=40, val3=30;
+		System.out.println("Maximun number is "+maximum(val1,val2,val3));
 
 	}
 
