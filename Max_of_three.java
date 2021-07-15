f1309e46ec298f18682c9101c44bcb950aacd006
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-16 00:56:38 +0530

    Refactored: code using generics

--- Max_of_three.java
+++ Max_of_three.java
@@ -1,9 +1,9 @@
 package maxOfThree;
 
-public class Max_of_three {
+public class Max_of_three<T> {
 	
-	public static String maximum(String val1, String val2, String val3) {
-		String max = val1;
+	public static <T extends Comparable <T>> T maximum (T val1, T val2, T val3) {
+		T max = val1;
 		if(val2.compareTo(max)>0) {
 			max=val2;
 		}else if(val3.compareTo(max)>0) {
@@ -13,8 +13,9 @@ public class Max_of_three {
 	}
 	public static void main(String[] args) {
 		String val1="Banana", val2= "Peach", val3="Apple";
+		Integer num1 =10, num2 =20, num3 =40;
 		System.out.println("Maximun is "+maximum(val1,val2,val3));
-
+		System.out.println("Maximun is "+maximum(num1, num2, num3));
 	}
 
 }
