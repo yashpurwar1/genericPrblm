3e6a26b0cd5208c38da711ed1fb743f031a1219d
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-16 00:46:19 +0530

    Refactored: code for max of string

--- Max_of_three.java
+++ Max_of_three.java
@@ -2,8 +2,8 @@ package maxOfThree;
 
 public class Max_of_three {
 	
-	public static Float maximum(Float val1, Float val2, Float val3) {
-		Float max = val1;
+	public static String maximum(String val1, String val2, String val3) {
+		String max = val1;
 		if(val2.compareTo(max)>0) {
 			max=val2;
 		}else if(val3.compareTo(max)>0) {
@@ -12,8 +12,8 @@ public class Max_of_three {
 		return max;
 	}
 	public static void main(String[] args) {
-		Float val1=(float) 20.0, val2=(float) 40.0, val3=(float)30.0;
-		System.out.println("Maximun number is "+maximum(val1,val2,val3));
+		String val1="Banana", val2= "Peach", val3="Apple";
+		System.out.println("Maximun is "+maximum(val1,val2,val3));
 
 	}
 
