10d53ea6e502234ac94964dd9cb82f4cfb0f38b0
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-16 01:29:05 +0530

    Refactored: code using generics class and constructor

--- Max_of_three.java
+++ Max_of_three.java
@@ -1,8 +1,15 @@
 package maxOfThree;
 
-public class Max_of_three<T> {
+public class Max_of_three<T extends Comparable <T>> {
+	T val1, val2, val3;
 	
-	public static <T extends Comparable <T>> T maximum (T val1, T val2, T val3) {
+	public Max_of_three(T val1, T val2, T val3) {
+		this.val1=val1;
+		this.val2=val2;
+		this.val3=val3;
+	}
+	
+	public T maximum (T val1, T val2, T val3) {
 		T max = val1;
 		if(val2.compareTo(max)>0) {
 			max=val2;
@@ -12,10 +19,8 @@ public class Max_of_three<T> {
 		return max;
 	}
 	public static void main(String[] args) {
-		String val1="Banana", val2= "Peach", val3="Apple";
-		Integer num1 =10, num2 =20, num3 =40;
-		System.out.println("Maximun is "+maximum(val1,val2,val3));
-		System.out.println("Maximun is "+maximum(num1, num2, num3));
+		Max_of_three obj = new Max_of_three("yash","Ankur","jatin");
+		System.out.println("Maximun is "+obj.maximum(obj.val1, obj.val2, obj.val3));
 	}
 
 }
