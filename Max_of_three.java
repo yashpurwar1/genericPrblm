0c5db96f7d9c64559ed7b030b852ca0062ba708b
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-16 01:57:25 +0530

    Refactored: code using array

--- Max_of_three.java
+++ Max_of_three.java
@@ -1,26 +1,27 @@
 package maxOfThree;
 
+import java.util.Arrays;
+
 public class Max_of_three<T extends Comparable <T>> {
-	T val1, val2, val3;
+	T[] valueArray;
 	
-	public Max_of_three(T val1, T val2, T val3) {
-		this.val1=val1;
-		this.val2=val2;
-		this.val3=val3;
+	public Max_of_three(T[] valueArray) {
+		this.valueArray= valueArray;
 	}
 	
-	public T maximum (T val1, T val2, T val3) {
-		T max = val1;
-		if(val2.compareTo(max)>0) {
-			max=val2;
-		}else if(val3.compareTo(max)>0) {
-			max=val3;
-		}
+	public T maximum (T[] valueArray) {
+		Arrays.sort(valueArray);
+		int len = valueArray.length;
+		T max = valueArray[len-1];
 		return max;
 	}
 	public static void main(String[] args) {
-		Max_of_three obj = new Max_of_three("yash","Ankur","jatin");
-		System.out.println("Maximun is "+obj.maximum(obj.val1, obj.val2, obj.val3));
+		Comparable[] intArray = {1,5,8,6};
+		Max_of_three test1 = new Max_of_three(intArray);
+		System.out.println("Maximun is "+test1.maximum(intArray));
+		Comparable[] stringArray = {"yash", "ankur", "saket"};
+		Max_of_three test2 = new Max_of_three(stringArray);
+		System.out.println("Maximun is "+test2.maximum(stringArray));
 	}
 
 }
